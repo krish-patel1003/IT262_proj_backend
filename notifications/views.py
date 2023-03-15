14ef4ae79e0cf02c86ea3764b73bdb47caee6d29
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-03-15 14:48:39 +0530

    emergency leave detection added

--- notifications/views.py
+++ notifications/views.py
@@ -7,7 +7,8 @@ from .models import Notice
 from .serializers import NoticeSerializer
 from .permissions import NoticePermission
 from .signals import emergency_leave
-from datetime import datetime
+from appointments.utils import parse_date
+from datetime import datetime, date
 # Create your views here.
 
 class NoticeViewSet(ModelViewSet):
@@ -16,13 +17,29 @@ class NoticeViewSet(ModelViewSet):
     queryset = Notice.objects.all()
     permission_classes = [IsAuthenticated, NoticePermission]
 
-    # def create(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         
-    #     # data = request.data
-    #     # if data["tag"] == "Leave":
-    #     #     if data["on_leave_from"] == datetime.today() or data["on_leave_till"] == datetime.today():
-    #     #         print("Emergency Leave")
-    #     pass
+        data = request.data
+        on_leave_from = date(*parse_date(data["on_leave_from"]))
+        on_leave_till = date(*parse_date(data["on_leave_till"]))
+        today = datetime.today().date()
+
+        print("values - >", on_leave_from, on_leave_till, today)
+        if data["tag"] == "Leave":
+            if on_leave_from == today:
+                print("Emergency Leave")
+
+                if on_leave_till and on_leave_till > today:
+                    print("emergency leave of multiple days")
+            
+                if on_leave_till and on_leave_till == today:
+                    print("emergency leave of one day")
+            
+            else:
+                print("Leave declared in advanced")
+        
+        return super().create(self, request, *args, **kwargs)
+
 
     
 
