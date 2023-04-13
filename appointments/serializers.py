c12decfb2adafc26deb9d4f4c1d07e6589525940
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-13 14:34:42 +0530

    appointment get response changed

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Symptom, Prescription, Appointment
 from users.models import StudentProfile
+from rest_framework import serializers
 
 
 class SymptomSerializer(ModelSerializer):
@@ -17,6 +18,8 @@ class PrescriptionSerializer(ModelSerializer):
 
 class AppointmentSerializer(ModelSerializer):
 
+    # student_data = serializers.SerializerMethodField("get_student_data")
+
     class Meta:
         model = Appointment
         fields = (
@@ -30,9 +33,20 @@ class AppointmentSerializer(ModelSerializer):
             "status"
         )
         extra_kwargs = {
-            "student":{"read_only":True}
+            "student":{"read_only":True},
         }
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        student_id = instance.student.id
+        student_data = StudentProfile.objects.get(id=student_id)
+        name = student_data.user.get_full_name()
+        representation["student_data"] = {
+            "name": name,
+            "rollno": student_data.rollno
+        }
+
+        return representation
     def create(self, attrs):
         student = StudentProfile.objects.get(user=self.context["request"].user)
         attrs["student"] = student
