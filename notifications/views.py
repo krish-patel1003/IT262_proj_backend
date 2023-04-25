097bcb920f2bb58ce801939eb33463dfe177f1bd
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-26 01:25:25 +0530

    reschedule appointment fubction added

--- notifications/views.py
+++ notifications/views.py
@@ -8,7 +8,8 @@ from .serializers import NoticeSerializer
 from .permissions import NoticePermission
 from .signals import emergency_leave
 from appointments.utils import parse_date
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
+from appointments.models import Appointment
 # Create your views here.
 
 class NoticeViewSet(ModelViewSet):
@@ -16,6 +17,15 @@ class NoticeViewSet(ModelViewSet):
     serializer_class = NoticeSerializer
     queryset = Notice.objects.all()
     permission_classes = [IsAuthenticated, NoticePermission]
+
+    def reschedule(self, on_leave_from, on_leave_till):
+        print("rescheduling")
+        appointments = Appointment.objects.filter(date=on_leave_from)
+        for appointment in appointments:
+            print("appointments: ", appointment.date)
+            appointment.save(date=on_leave_till+timedelta(days=1))
+            print("rescheduled apmt: ", appointment.date)
+        
 
     def create(self, request, *args, **kwargs):
         print(request.data)
@@ -31,15 +41,17 @@ class NoticeViewSet(ModelViewSet):
 
                 if on_leave_till and on_leave_till > today:
                     print("emergency leave of multiple days")
+                    self.reschedule(today, on_leave_till)
             
                 if on_leave_till and on_leave_till == today:
                     print("emergency leave of one day")
-            
+                    self.reschedule(today, on_leave_till)
             else:
                 print("Leave declared in advanced")
+                self.reschedule(on_leave_from, on_leave_till)
         
         return super().create(request, *args, **kwargs)
 
 
-    
+
 
