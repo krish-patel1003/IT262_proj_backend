0341e807e05b1c371899e4720b7d7871a87888ed
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-26 13:31:32 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'users.middlewares.CustomHeaderMiddleware',
 ]
 
