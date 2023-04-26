3bcf1b97bb2ef836dd79802d75005fa0943f9d90
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-26 12:38:34 +0530

    changes

--- users/student_views/permissions.py
+++ users/student_views/permissions.py
@@ -13,9 +13,5 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
-
-        if request.method in permissions.SAFE_METHODS:
-            """Allows users to view data but not edit"""
-            return True
-        
+            
         return obj.user == request.user
